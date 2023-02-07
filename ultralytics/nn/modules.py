c754664dd0374f59f167e089daf82b0381e0a11e
ldbang <95518835+ldbang@users.noreply.github.com>
2023-02-08 02:42:38 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -107,36 +107,36 @@ class DWConv(Conv):
         self.conv=Conv(c1, c2, k, s, g=math.gcd(c1, c2), act=act)
 # def DWConv(c1, c2, k=1, s=1, act=True):
 #     # Depthwise convolution
-    def forward_fuse(self, x):
+    def forward(self, x):
         return self.conv(x)
     
-class DepthWiseConv(nn.Module):
-    def __init__(self,in_channel,out_channel,k,s,act):
+# class DepthWiseConv(nn.Module):
+#     def __init__(self,in_channel,out_channel,k,s,act):
  
-        #这一行千万不要忘记
-        super(DepthWiseConv, self).__init__()
+#         #这一行千万不要忘记
+#         super(DepthWiseConv, self).__init__()
  
-        # 逐通道卷积
-        self.depth_conv = nn.Conv2d(in_channels=in_channel,
-                                    out_channels=in_channel,
-                                    kernel_size=3,
-                                    stride=1,
-                                    padding=1,
-                                    groups=in_channel)
-        # groups是一个数，当groups=in_channel时,表示做逐通道卷积
+#         # 逐通道卷积
+#         self.depth_conv = nn.Conv2d(in_channels=in_channel,
+#                                     out_channels=in_channel,
+#                                     kernel_size=3,
+#                                     stride=1,
+#                                     padding=1,
+#                                     groups=in_channel)
+#         # groups是一个数，当groups=in_channel时,表示做逐通道卷积
  
-        #逐点卷积
-        self.point_conv = nn.Conv2d(in_channels=in_channel,
-                                    out_channels=out_channel,
-                                    kernel_size=1,
-                                    stride=1,
-                                    padding=0,
-                                    groups=1)
+#         #逐点卷积
+#         self.point_conv = nn.Conv2d(in_channels=in_channel,
+#                                     out_channels=out_channel,
+#                                     kernel_size=1,
+#                                     stride=1,
+#                                     padding=0,
+#                                     groups=1)
     
-   def forward(self,input):
-        out = self.depth_conv(input)
-        out = self.point_conv(out)
-        return out
+#    def forward(self,input):
+#         out = self.depth_conv(input)
+#         out = self.point_conv(out)
+#         return out
 class DWConvTranspose2d(nn.ConvTranspose2d):
     # Depth-wise transpose convolution
     def __init__(self, c1, c2, k=1, s=1, p1=0, p2=0):  # ch_in, ch_out, kernel, stride, padding, padding_out
