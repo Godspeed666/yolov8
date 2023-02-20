cb483f9bc0308d5b6701898dbac9bb95bfc9081f
ldbang <95518835+ldbang@users.noreply.github.com>
2023-02-20 17:23:20 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -385,7 +385,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in {
                 Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF1, SPPF,SimSPPF,SimCSPSPPF,CSPSPPF,DWConv, Focus,RepBottleneck,RepC3,RepC2f,
-                BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x,SE,ECA,SimAM,C2f_CABM,C2f_rep,CBAM,CBAM_2,RepConv,GhostC2f}:
+                BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x,SE,ECA,SimAM,C2f_CABM,C2f_rep,CBAM,CBAM_2,RepConv,GhostC2f,Bi}:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
                 c2 = make_divisible(c2 * gw, 8)
