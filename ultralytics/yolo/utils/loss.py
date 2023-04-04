0c1c3a4e6be440f133da5185bddaf179bd16dbc2
ldbang <95518835+ldbang@users.noreply.github.com>
2023-04-05 01:26:02 +0800

    Update loss.py

--- ultralytics/yolo/utils/loss.py
+++ ultralytics/yolo/utils/loss.py
@@ -8,6 +8,35 @@ from .metrics import bbox_iou
 from .tal import bbox2dist
 
 
+################################################################
+def wasserstein_loss(pred, target, eps=1e-7, constant=12.8):
+    """Implementation of paper `A Normalized Gaussian Wasserstein Distance for
+    Tiny Object Detection <https://arxiv.org/abs/2110.13389>.
+    Args:
+        pred (Tensor): Predicted bboxes of format (cx, cy, w, h),
+            shape (n, 4).
+        target (Tensor): Corresponding gt bboxes, shape (n, 4).
+        eps (float): Eps to avoid log(0).
+    Return:
+        Tensor: Loss tensor.
+    """
+    center1 = pred[:, :2]
+    center2 = target[:, :2]
+    whs = center1[:, :2] - center2[:, :2]
+    center_distance = whs[:, 0] * whs[:, 0] + whs[:, 1] * whs[:, 1] + eps
+ 
+    w1 = pred[:, 2] + eps
+    h1 = pred[:, 3] + eps
+    w2 = target[:, 2] + eps
+    h2 = target[:, 3] + eps
+ 
+    wh_distance = ((w1 - w2) ** 2 + (h1 - h2) ** 2) / 4
+    wasserstein_2 = center_distance + wh_distance
+    return torch.exp(-torch.sqrt(wasserstein_2) / constant)
+
+###################################################################
+
+
 class VarifocalLoss(nn.Module):
     # Varifocal loss by Zhang et al. https://arxiv.org/abs/2008.13367
     def __init__(self):
@@ -32,7 +61,16 @@ class BboxLoss(nn.Module):
         # IoU loss
         weight = torch.masked_select(target_scores.sum(-1), fg_mask).unsqueeze(-1)
         iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
-        loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+#         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+        
+        nwd_ratio = 0.5  # 平衡nwd和原始iou的权重 
+        nwd = wasserstein_loss(pred_bboxes[fg_mask], target_bboxes[fg_mask]).squeeze()
+        loss_iou += (1 - nwd_ratio) * (1.0 - nwd).mean() + nwd_ratio * (1.0 - iou).mean()  # iou loss
+
+
+        
+         
+        
         
         # DFL loss
         if self.use_dfl:
